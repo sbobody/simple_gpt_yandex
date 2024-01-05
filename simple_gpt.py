7a09d09c8e9335ac95519d53e4ee3bf62e00287d
aktec666 <aktec@mail.ru>
2024-01-05 09:12:53 +0300

    first commit

---
+++ simple_gpt.py
@@ -0,0 +1,35 @@
+import requests
+
+prompt = {
+    "modelUri": "gpt://<id>/yandexgpt-lite",
+    "completionOptions": {
+        "stream": False,
+        "temperature": 0.6,
+        "maxTokens": "2000"
+    },
+    "messages": [
+        {
+            "role": "system",
+            "text": "Ты ассистент дроид, способный помочь в галактических приключениях."
+        },
+        {
+            "role": "user",
+            "text": "Привет, Дроид! Мне нужна твоя помощь, чтобы узнать больше о Силе. Как я могу научиться ее использовать?"
+        },
+        {
+            "role": "assistant",
+            "text": "Привет! Чтобы овладеть Силой, тебе нужно понять ее природу. Сила находится вокруг нас и соединяет всю галактику. Начнем с основ медитации."
+        }
+    ]
+}
+
+
+url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Api-Key <key>"
+}
+
+response = requests.post(url, headers=headers, json=prompt)
+result = response.json().get('result')
+print(result['alternatives'][0]['message']['text'])
